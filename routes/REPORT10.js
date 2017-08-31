1ad0ba292707c9915a50b20f39f8a0ea1b265201
at3158 <tk290@tkfood.com.tw>
2017-08-31 11:40:36 +0800

    modif date

--- routes/REPORT10.js
+++ routes/REPORT10.js
@@ -28,7 +28,7 @@ router.post('/', function(req, res){
             var SQL;
             var request = new sql.Request();
 
-            SQL="SELECT TH004 AS '品號',TH005 AS '品名',CONVERT(real, SUM(TH008)) AS '數量',CONVERT(real, SUM(TH024)) AS '贈品',TH009 AS '單位',CONVERT(real, SUM(TH013)) AS '金額' FROM [TK].dbo.COPTG,[TK].dbo.COPTH WHERE TG001=TH001 AND TG002=TH002 AND (TG001='A233'  OR (TG001='A230'  AND TG006  IN ('160092','170007') )) AND TG003>='20170801' AND TG003<='20170831' GROUP BY TH004,TH005,TH009 ORDER BY SUM(TH013) DESC";
+            SQL="SELECT TH004 AS '品號',TH005 AS '品名',CONVERT(real, SUM(TH008)) AS '數量',CONVERT(real, SUM(TH024)) AS '贈品',TH009 AS '單位',CONVERT(real, SUM(TH013)) AS '金額' FROM [TK].dbo.COPTG,[TK].dbo.COPTH WHERE TG001=TH001 AND TG002=TH002 AND (TG001='A233'  OR (TG001='A230'  AND TG006  IN ('160092','170007') )) AND TG003>='"+req.body.date1+"' AND TG003<='"+req.body.date2+"' GROUP BY TH004,TH005,TH009 ORDER BY SUM(TH013) DESC";
 
             // query to the database and get the records
             request.query(SQL
