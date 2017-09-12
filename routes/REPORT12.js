d9b140d1a1e6e00ef3d3f474b87c4b78a8efe962
at3158 <tk290@tkfood.com.tw>
2017-09-12 10:56:44 +0800

    modify sql

--- routes/REPORT12.js
+++ routes/REPORT12.js
@@ -21,7 +21,7 @@ router.get('/', function(req, res) {
             var SQL;
             var request = new sql.Request();
 
-            SQL=" SELECT [ID],[MB001],[TYPEWEIGHT],[TYPELENGTH],[TYPEWIDTH],[COOKWEIGHT],[COOKLENGTH],[COOKWIDTH] FROM [TKMOC].[dbo].[PRODUCTCHECK] ";
+            SQL=" SELECT [MANU] AS '線別',CONVERT(varchar(100),[MANUDATE],112) AS '生產日',[MOCMANULINE].[MB001] AS '品號',[MB002] AS '品名',[MB003] AS '規格',[BAR] AS '桶數',[NUM] AS '數量',[CLINET] AS '客戶',[TYPEWEIGHT] AS '成型重量',[TYPELENGTH] AS '成型長度',[TYPEWIDTH] AS '成型寬度',[COOKWEIGHT] AS '烘烤重量',[COOKLENGTH] AS '烘烤長度',[COOKWIDTH] AS '烘烤寬度' FROM [TKMOC].[dbo].[MOCMANULINE]LEFT JOIN [TKMOC].[dbo].[PRODUCTCHECK] ON [MOCMANULINE].[MB001]=[PRODUCTCHECK].[MB001] WHERE  [MANU] IN('新廠製二組','新廠製一組')  AND CONVERT(varchar(100),[MANUDATE],112) LIKE +''+CONVERT(NVARCHAR,GETDATE(),112)+'%' ORDER BY [MANUDATE],[MANU],[SERNO] ";
 
             // query to the database and get the records
             request.query(SQL
